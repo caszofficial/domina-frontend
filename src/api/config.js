c2dcdba9874db1106c2ce0d0bfcd95a4d0a7964f
Santiago Cañas Zapata <77809578+caszofficial@users.noreply.github.com>
2025-11-01 06:37:08 -0500

    Fix tasks fetch to avoid cached responses

--- src/api/config.js
+++ src/api/config.js
@@ -2,6 +2,8 @@ export const apiConfig = {
   authBaseUrl: `${import.meta.env.VITE_AUTH_SERVICE_URL}/api/users`,
   tasksBaseUrl: `${import.meta.env.VITE_TASKS_SERVICE_URL}/api/tasks`,
   defaultHeaders: {
-    "Content-Type": "application/json",
-  },
-};
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache'
+  }
+}
